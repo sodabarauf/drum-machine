b0700e6cfbb3d4ed86b7549f153f72f55d5a7fa7
sodabarauf <sodabarauf4@gmail.com>
2024-08-21 18:01:30 +0430

    fixed error

--- src/App.js
+++ src/App.js
@@ -1,59 +1,59 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
+
 import './App.css';
 function App() {
   const [activeKey, setActiveKey] = useState('');
   useEffect(() => {
     document.addEventListener('keydown', (event) => {
-      console.log(event.key)
       playSound(event.key.toUpperCase());
     });
   }, []);
   const drumPads = [
     {
       keyCode: 81,
-      text: "Q",
-      src: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-1.mp3"
+      text: 'Q',
+      src: 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-1.mp3',
     },
     {
       keyCode: 87,
-      text: "W",
-      src: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-2.mp3"
+      text: 'W',
+      src: 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-2.mp3',
     },
     {
       keyCode: 69,
-      text: "E",
-      src: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-3.mp3"
+      text: 'E',
+      src: 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-3.mp3',
     },
     {
       keyCode: 65,
-      text: "A",
-      src: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-4_1.mp3"
+      text: 'A',
+      src: 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-4_1.mp3',
     },
     {
       keyCode: 83,
-      text: "S",
-      src: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-6.mp3"
+      text: 'S',
+      src: 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-6.mp3',
     },
     {
       keyCode: 68,
-      text: "D",
-      src: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Dsc_Oh.mp3"
+      text: 'D',
+      src: 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/Dsc_Oh.mp3',
     },
     {
       keyCode: 90,
-      text: "Z",
-      src: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/RP4_KICK_1.mp3"
+      text: 'Z',
+      src: 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/RP4_KICK_1.mp3',
     },
     {
       keyCode: 88,
-      text: "X",
-      src: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Kick_n_Hat.mp3"
+      text: 'X',
+      src: 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/Kick_n_Hat.mp3',
     },
     {
       keyCode: 67,
-      text: "C",
-      src: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Cev_H2.mp3"
-    },
+      text: 'C',
+      src: 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/Cev_H2.mp3',
+    },
   ];
 
   function playSound(selector) {
@@ -61,7 +61,7 @@ function App() {
     console.log(audio);
     audio.play();
     setActiveKey(selector)
-  }
+  };
 
   return (
     <div className="App">
@@ -75,8 +75,7 @@ function App() {
                 playSound(drumPad.text);
               }}
               className="drum-pad"
-              id={drumPad.src}
-            >
+              id={drumPad.src}>
               {drumPad.text}
               <audio
                 className="clip"
@@ -89,8 +88,7 @@ function App() {
       </div>
     </div>
   );
-  
-  }
+
+}
 
 export default App;
-
